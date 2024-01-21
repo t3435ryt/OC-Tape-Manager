4ddfa84f8495a896e601ddf28c15b1cc1d68c2d7
t3435ryt <154918159+t3435ryt@users.noreply.github.com>
2024-01-21 17:06:12 -0500

    add components

--- src/tapeman.lua
+++ src/tapeman.lua
@@ -1,6 +1,13 @@
 
 local arguments_as_a_table = {}
 
+local tape1 = component.proxy("ed6f617e-fc8e-405b-877e-eafbe97fa116")
+local tape2 = component.proxy("a49e98dc-dcb2-49fd-84d3-5927573b4191")
+local transposer = component.proxy("6208f201-e283-4c8e-8ad5-df470c327d8e")
+
+
+
+
 for index, value in ipairs(arguments_as_a_table) do
     print(value)
 end
