36b0760813faf606ec91e47bb35d47327c78a84b
CGMcKeever1 <109530432+CGMcKeever1@users.noreply.github.com>
2024-01-21 18:15:56 -0500

    test function to get stored tapes

--- src/tapeman.lua
+++ src/tapeman.lua
@@ -7,7 +7,10 @@ local tape1 = component.proxy("ed6f617e-fc8e-405b-877e-eafbe97fa116")
 local tape2 = component.proxy("a49e98dc-dcb2-49fd-84d3-5927573b4191")
 local transposer = component.proxy("6208f201-e283-4c8e-8ad5-df470c327d8e")
 
+local function getStoredTapes()
+    return transposer.getAllStacks(1).getAll()
+end
 
-for index, value in ipairs(arguments_as_a_table) do
+for index, value in ipairs(getStoredTapes()) do
     print(value)
 end
